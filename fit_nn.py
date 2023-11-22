d2e7fc6ba20c3d8dbe6e07d0f2114880e53d0d54
Neel-132 <neelabha.12.banerjee@gmail.com>
2023-11-22 15:05:39 +0530

    final_model

--- fit_nn.py
+++ fit_nn.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader, TensorDataset, random_split
+from torcheval.metrics.functional import multiclass_f1_score
+from imblearn.under_sampling import RandomUnderSampler
 from tqdm.auto import trange
 import torch.optim as optim
 from statistics import mean
@@ -9,16 +11,27 @@ import sys
 import pickle
 PTH = 'C:\\Users\\Neela\\Documents\\GitHub\\EntityAspectLinking\\picklefiles'
 CKP = 'C:\\Users\\Neela\\Documents\\GitHub\\EntityAspectLinking\\checkpoint\\'
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = 'cpu'
 class Network(nn.Module):
-	def __init__(self, inp, hidden_1 = 256, hidden_2 = 128, hidden_3 = 32, device = None):
+
+	def __init__(self, inp, hidden_1 = 256, hidden_2 = 128, hidden_3 = 64, device = None):
+
 		super(Network, self).__init__()
-		self.model = nn.Sequential(nn.Linear(inp, hidden_1, bias = True),  nn.LeakyReLU(),
-			nn.Linear(hidden_1, hidden_2, bias = True), nn.LeakyReLU(), nn.Linear(hidden_2, hidden_3, bias = True),
+		self.device = device
+		self.model = nn.Sequential(nn.Linear(inp, hidden_1, bias = True),  
+			nn.LeakyReLU(),
+			nn.Dropout(0.4),
+			nn.Linear(hidden_1, hidden_2, bias = True), 
+			nn.LeakyReLU(), 
+			nn.Dropout(0.8),
+			nn.Linear(hidden_2, hidden_3, bias = True),
 			nn.Linear(hidden_3, 1))
+		self.model.to(device)
 		self.apply(self._init_weights)
 
 	def _init_weights(self, module):
+
 		if isinstance(module, nn.Linear):
 			module.weight = torch.nn.parameter.Parameter(
                 nn.init.kaiming_normal_(module.weight,
@@ -35,18 +48,17 @@ class Network(nn.Module):
 def prepare_dataset(file, train_ratio, val_ratio, batch_size,):
 	with open(f'{PTH}\\{file}', 'rb') as f:
 		data = pickle.load(f)
-	f.close()
-	
-	size = data.shape[0]
-	train_size = int(train_ratio * size)
-	val_size = int(val_ratio * size)
-	test_size = size - train_size - val_size
-	data = torch.tensor(data)	
+	f.close()	
 	features = data[ : , : -1]
 	#print(features.shape)
 	target = data[:, -1]
-	#print(len(target))
-	data = TensorDataset(features, target)	
+	undersample = RandomUnderSampler(sampling_strategy='majority')
+	features, target = undersample.fit_resample(features, target)
+	data = TensorDataset(torch.tensor(features), torch.tensor(target))
+	size = features.shape[0]
+	train_size = int(train_ratio * size)
+	val_size = int(val_ratio * size)
+	test_size = size - train_size - val_size
 	train_dataset, val_dataset, test_dataset = random_split(data, [train_size, val_size, test_size])
 	train_loader = DataLoader(train_dataset, batch_size = batch_size, shuffle = True)
 	val_loader = DataLoader(val_dataset, batch_size = batch_size, shuffle = True)
@@ -56,6 +68,7 @@ def prepare_dataset(file, train_ratio, val_ratio, batch_size,):
 
 def evaluate(dataloader, model, loss):
 	runningloss = 0
+	f1sc = 0
 	for data in dataloader:
 		x, t = data
 		x = x.to(device, dtype = torch.float32)
@@ -64,8 +77,14 @@ def evaluate(dataloader, model, loss):
 		pred = model(x)
 		pred = pred.view(t.shape)
 		ls = loss(pred, t)
+		pred_class = [1 if item > 0.5 else 0 for item in pred]
+		pred_class = torch.FloatTensor(pred_class)
+		pred_class.to(device)
+		#print(pred_class, t)
+		f1_score = multiclass_f1_score(pred_class, t, num_classes=2)
+		f1sc += f1_score 
 		runningloss += ls.item()
-	return runningloss / len(dataloader) 
+	return runningloss / len(dataloader), f1sc / len(dataloader) 
 		
 
 
@@ -83,9 +102,13 @@ def train(train_ratio, val_ratio, batch_size, epochs, lr = 0.001, weight_decay =
 	lr_scheduler = optim.lr_scheduler.ReduceLROnPlateau(
             opt, mode='min', factor=0.5, patience=4)
 	for epoch in trange(epochs):
+		model.train()
+		model.to(device)
 		if epoch == 0:
 			print('Epoch %d / %d : Train Loss = %.6f, Val Loss = %.6f, Test Loss = %.6f'
-				% (epoch, epochs, evaluate(train_loader, model, loss_fn), evaluate(val_loader, model, loss_fn), evaluate(test_loader, model, loss_fn)))
+				% (epoch, epochs, evaluate(train_loader, model, loss_fn)[0], evaluate(val_loader, model, loss_fn)[0], evaluate(test_loader, model, loss_fn)[0]))
+
+			print('Epoch: %d / %d, F_1 score : %0.6f' %(epoch, epochs, evaluate(test_loader, model, loss_fn)[1]))
 
 		start = len(loss_val)
 		for i, data in enumerate(train_loader, 0):
@@ -102,8 +125,9 @@ def train(train_ratio, val_ratio, batch_size, epochs, lr = 0.001, weight_decay =
 		end = len(loss_val)
 		avg_loss_epoch.append(mean(loss_val[start : end + 1]))
 		with torch.no_grad():
-			trainloss = evaluate(train_loader, model, loss_fn)
-			valloss = evaluate(train_loader, model, loss_fn)
+			model.eval()
+			trainloss = evaluate(train_loader, model, loss_fn)[0]
+			valloss = evaluate(train_loader, model, loss_fn)[0]
 			if valloss < best_devloss:
 				best_devloss = valloss
 				bestepoch = epoch
@@ -122,10 +146,10 @@ def train(train_ratio, val_ratio, batch_size, epochs, lr = 0.001, weight_decay =
 			print("Early stopping")
 			break
 
-	print('Epoch: %d/%d, Train loss: %0.6f, Valid loss: %0.6f, Test loss:%0.6f' %
-		(epoch, epochs, evaluate(train_loader, model, loss_fn), evaluate(val_loader, model, loss_fn), evaluate(test_loader, model, loss_fn)))
+	print('Epoch: %d / %d, Train loss: %0.6f, Valid loss: %0.6f, Test loss:%0.6f' %
+		(epoch, epochs, evaluate(train_loader, model, loss_fn)[0], evaluate(val_loader, model, loss_fn)[0], evaluate(test_loader, model, loss_fn)[0]))
 
-
+	print('Epoch: %d / %d, F_1 score : %0.6f' %(epoch, epochs, evaluate(test_loader, model, loss_fn)[1]))
 		
 
 
@@ -136,4 +160,5 @@ for data in train_loader:
 	x, y = data
 	x = x.to(device)
 	print(x.size())'''
-train(0.6, 0.1, 32, 200)
+train(0.8, 0.1, 128, 100)
+
